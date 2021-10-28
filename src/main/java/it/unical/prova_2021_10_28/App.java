17ac7983cd84384a460ed727ffb1faa058ea612b
Gerardo Napoli <gerry.nap98@gmail.com>
2021-10-28 09:03:22 +0200

    initial commit

---
+++ src/main/java/it/unical/prova_2021_10_28/App.java
@@ -0,0 +1,13 @@
+package it.unical.prova_2021_10_28;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
